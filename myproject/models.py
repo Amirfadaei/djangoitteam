6cc6acf9cf9cb7a25b61c58a0737dabda62e8e0c
amir <amirfadaei03@gmail.com>
2022-07-03 16:35:51 +0430

    admin_panel
    syntax_error

--- myproject/models.py
+++ myproject/models.py
@@ -1,3 +1,19 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=50, null=True, blank=True)
+    source_link = models.CharField(max_length=50, null=True, blank=True)
+    create = models.DateTimeField(auto_now_add=True)
+    # id = models.UUIDField(default=uuid.uuid4(), unique=True,primary_key=True, editable=False)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+
+
+    def __str__(self):
+        return self.title

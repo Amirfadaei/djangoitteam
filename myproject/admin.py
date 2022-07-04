5ff58fcb57595b256a27249932e422116f2d942d
amir <amirfadaei03@gmail.com>
2022-07-04 17:06:13 +0430

    Work-On-Relationships
    One-To-One
    One-To-Many
    Many-To-One

--- myproject/admin.py
+++ myproject/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import Projects
+
+
+from.models import Projects, Review, Tag
+
+# from.models import Test
+# from.models import Amir
 
 admin.site.register(Projects)
+admin.site.register(Review)
+admin.site.register(Tag)
+
+
+# admin.site.register(Test)
+# admin.site.register(Amir)
--- myproject/models.py
+++ myproject/models.py
@@ -1,19 +1,48 @@
 import uuid
-
 from django.db import models
 
 class Projects(models.Model):
-
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=500)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=50, null=True, blank=True)
     source_link = models.CharField(max_length=50, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
     create = models.DateTimeField(auto_now_add=True)
-    # id = models.UUIDField(default=uuid.uuid4(), unique=True,primary_key=True, editable=False)
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-
-
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # owner =
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    create = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    create = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    def __str__(self):
+        return self.name
+#
+# class Amir(models.Model):
+#     field = models.CharField(max_length=100, null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.field
+#
+#     def __str__(self):
+#         return self.new
